27820bb2de7ae12f6585abd88684aa2ae8c43cfb
Jorge Noel Gomez Coronado <jgomezc32@miumg.edu.gt>
2025-11-05 20:07:41 -0600

    .

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,26 +1,24 @@
 from django.contrib import admin
-from .models import Producto, Cliente, Venta, DetalleVenta, LogAccion
+from .models import Producto, Cliente, Venta, DetalleVenta
 
-class DetalleInline(admin.TabularInline):
+class DetalleVentaInline(admin.TabularInline):
     model = DetalleVenta
     extra = 0
 
-@admin.register(Venta)
-class VentaAdmin(admin.ModelAdmin):
-    list_display = ("id", "fecha", "cliente", "total")
-    inlines = [DetalleInline]
-
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
-    list_display = ("nombre","tipo","precio_litro","stock_litros","activo")
+    list_display = ("nombre", "tipo", "precio_por_litro", "stock", "activo")
+    list_filter = ("tipo", "activo")
     search_fields = ("nombre",)
 
 @admin.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
-    list_display = ("nombres","apellidos","telefono","nit")
-    search_fields = ("nombres","apellidos","nit")
+    list_display = ("nombre", "telefono", "nit")
+    search_fields = ("nombre", "nit")
 
-@admin.register(LogAccion)
-class LogAccionAdmin(admin.ModelAdmin):
-    list_display = ("fecha_hora","evento","modelo","pk_obj","usuario")
-    readonly_fields = ("fecha_hora","evento","modelo","pk_obj","payload","usuario")
+@admin.register(Venta)
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ("id", "cliente", "fecha", "total")
+    list_select_related = ("cliente",)
+    inlines = [DetalleVentaInline]
+
